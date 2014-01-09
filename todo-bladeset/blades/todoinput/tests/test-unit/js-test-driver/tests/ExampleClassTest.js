16a0b33d036c066b54caffbcac49b539b5532185
Phil Leggetter <phil@leggetter.co.uk>
2014-01-09 18:17:46 +0000

    update tests to use require

--- todo-bladeset/blades/todoinput/tests/test-unit/js-test-driver/tests/ExampleClassTest.js
+++ todo-bladeset/blades/todoinput/tests/test-unit/js-test-driver/tests/ExampleClassTest.js
@@ -1,56 +1,53 @@
-;( function() {
+var ServiceRegistry = require( 'br/ServiceRegistry' );
+var ExamplePresentationModel = require( 'brjstodo/todo/todoinput/ExamplePresentationModel' );
 
-  var ServiceRegistry = require( 'br/ServiceRegistry' );
+var fakeEventHub;
+var fakeChannel;
 
-  var fakeEventHub;
-  var fakeChannel;
-  
-  ExampleClassTest = TestCase('ExampleClassTest');
+var ExampleClassTest = TestCase('ExampleClassTest');
 
-  ExampleClassTest.prototype.setUp = function() {
+ExampleClassTest.prototype.setUp = function() {
 
-    fakeChannel = {
-      trigger: function( eventName, data ) {
-        // store event name and data
-        this.eventName = eventName;
-        this.data = data;
-      }
-    };
-
-    fakeEventHub = {
-      channel: function( channelName ) {
-        // store the name of the channel
-        this.channelName = channelName;
-        return fakeChannel;
-      }
-    };
-
-    // ensure there isn't already an event-hub registered
-    ServiceRegistry.deregisterService( 'br.demo-event-hub' );
-
-    // Register the fake event hub
-    ServiceRegistry.registerService( 'br.demo-event-hub', fakeEventHub );
+  fakeChannel = {
+    trigger: function( eventName, data ) {
+      // store event name and data
+      this.eventName = eventName;
+      this.data = data;
+    }
   };
 
-  ExampleClassTest.prototype.testTodoTextFieldIsInitialized = function() {
-    var todoInputBlade = new brjstodo.todo.todoinput.ExamplePresentationModel();
-
-    assertEquals( '', todoInputBlade.todoText.value.getValue() );
+  fakeEventHub = {
+    channel: function( channelName ) {
+      // store the name of the channel
+      this.channelName = channelName;
+      return fakeChannel;
+    }
   };
 
-  ExampleClassTest.prototype.testEnterKeyPressedTriggersEventOnEventHub = function() {
-    // Initialize
-    var testTodoTextValue = 'write some code and test it';
-    var todoInputBlade = new brjstodo.todo.todoinput.ExamplePresentationModel();
-    todoInputBlade.todoText.value.setValue( testTodoTextValue );
-  
-    // Execute test
-    todoInputBlade.keyPressed( null, { keyCode: 13 } );
-  
-    // Verify
-    assertEquals( 'todo-list', fakeEventHub.channelName );
-    assertEquals( 'todo-added', fakeChannel.eventName );
-    assertEquals( testTodoTextValue, fakeChannel.data.text );
-  };
+  // ensure there isn't already an event-hub registered
+  ServiceRegistry.deregisterService( 'br.demo-event-hub' );
 
-})();
+  // Register the fake event hub
+  ServiceRegistry.registerService( 'br.demo-event-hub', fakeEventHub );
+};
+
+ExampleClassTest.prototype.testTodoTextFieldIsInitialized = function() {
+  var todoInputBlade = new ExamplePresentationModel();
+
+  assertEquals( '', todoInputBlade.todoText.value.getValue() );
+};
+
+ExampleClassTest.prototype.testEnterKeyPressedTriggersEventOnEventHub = function() {
+  // Initialize
+  var testTodoTextValue = 'write some code and test it';
+  var todoInputBlade = new ExamplePresentationModel();
+  todoInputBlade.todoText.value.setValue( testTodoTextValue );
+
+  // Execute test
+  todoInputBlade.keyPressed( null, { keyCode: 13 } );
+
+  // Verify
+  assertEquals( 'todo-list', fakeEventHub.channelName );
+  assertEquals( 'todo-added', fakeChannel.eventName );
+  assertEquals( testTodoTextValue, fakeChannel.data.text );
+};
--- todo-bladeset/blades/todoitems/tests/test-unit/js-test-driver/tests/ExampleClassTest.js
+++ todo-bladeset/blades/todoitems/tests/test-unit/js-test-driver/tests/ExampleClassTest.js
@@ -1,59 +1,55 @@
-caplin.thirdparty( 'caplin-br' );
+var ServiceRegistry = require( 'br/ServiceRegistry' );
 
-( function() {
+var ExamplePresentationModel = require( 'brjstodo/todo/todoinput/ExamplePresentationModel' );
 
-    var ServiceRegistry = require( 'br/ServiceRegistry' );
+var fakeEventHub;
+var fakeChannel;
+    
+var ExampleClassTest = TestCase('ExampleClassTest');
 
-    var fakeEventHub;
-    var fakeChannel;
-        
-    ExampleClassTest = TestCase('ExampleClassTest');
+ExampleClassTest.prototype.setUp = function() {
 
-    ExampleClassTest.prototype.setUp = function() {
+  fakeChannel = {
+    on: function(eventName, callback, context) {
+      // store event name and data
+      this.eventName = eventName;
+      this.callback = callback;
+      this.context = context;
+    }
+  };
 
-      fakeChannel = {
-        on: function(eventName, callback, context) {
-          // store event name and data
-          this.eventName = eventName;
-          this.callback = callback;
-          this.context = context;
-        }
-      };
+  fakeEventHub = {
+    channel: function( channelName ) {
+      // store the name of the channel
+      this.channelName = channelName;
+      return fakeChannel;
+    }
+  };
 
-      fakeEventHub = {
-        channel: function( channelName ) {
-          // store the name of the channel
-          this.channelName = channelName;
-          return fakeChannel;
-        }
-      };
+  // ensure there isn't already an event-hub registered
+  ServiceRegistry.deregisterService( 'br.demo-event-hub' );
 
-      // ensure there isn't already an event-hub registered
-      ServiceRegistry.deregisterService( 'br.demo-event-hub' );
+  // Register the fake event hub
+  ServiceRegistry.registerService( 'br.demo-event-hub', fakeEventHub );
+};
 
-      // Register the fake event hub
-      ServiceRegistry.registerService( 'br.demo-event-hub', fakeEventHub );
-    };
+ExampleClassTest.prototype.testTodoItemsBladeListensToItemAddedEvents = function() {
+  var todoItemsBlade = new ExamplePresentationModel();
 
-    ExampleClassTest.prototype.testTodoItemsBladeListensToItemAddedEvents = function() {
-      var todoItemsBlade = new brjstodo.todo.todoitems.ExamplePresentationModel();
+  assertEquals( fakeEventHub.channelName , 'todo-list' );
+  assertEquals( fakeChannel.eventName , 'todo-added' );
+  assertEquals( fakeChannel.context , todoItemsBlade );
+};
 
-      assertEquals( fakeEventHub.channelName , 'todo-list' );
-      assertEquals( fakeChannel.eventName , 'todo-added' );
-      assertEquals( fakeChannel.context , todoItemsBlade );
-    };
+ExampleClassTest.prototype.testItemsViewModelAddsItemOnTodoAddedEvent = function() {
+  var todoItemsBlade = new ExamplePresentationModel();
 
-    ExampleClassTest.prototype.testItemsViewModelAddsItemOnTodoAddedEvent = function() {
-      var todoItemsBlade = new brjstodo.todo.todoitems.ExamplePresentationModel();
+  var itemText = 'hello';
 
-      var itemText = 'hello';
+  // trigger the callback
+  fakeChannel.callback.call( fakeChannel.context, { text: itemText } );
 
-      // trigger the callback
-      fakeChannel.callback.call( fakeChannel.context, { text: itemText } );
-
-      // check the item has been added to the end of the list
-      var items = todoItemsBlade.items.getPresentationNodesArray();
-      assertEquals( itemText, items[ items.length - 1 ].value.getValue() );
-    };
-
-})();
+  // check the item has been added to the end of the list
+  var items = todoItemsBlade.items.getPresentationNodesArray();
+  assertEquals( itemText, items[ items.length - 1 ].value.getValue() );
+};
