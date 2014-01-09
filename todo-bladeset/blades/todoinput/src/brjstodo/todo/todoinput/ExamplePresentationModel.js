a6839ee0519d2802d3b673b80f7d929456b45f90
Phil Leggetter <phil@leggetter.co.uk>
2014-01-09 18:05:13 +0000

    Updated to use node.js style code

--- todo-bladeset/blades/todoinput/src/brjstodo/todo/todoinput/ExamplePresentationModel.js
+++ todo-bladeset/blades/todoinput/src/brjstodo/todo/todoinput/ExamplePresentationModel.js
@@ -1,27 +1,24 @@
-( function() {
+var ENTER_KEY_CODE = 13;
 
-  var ENTER_KEY_CODE = 13;
+var br = require( 'br/Core' );
+var ServiceRegistry = require( 'br/ServiceRegistry' );
+var PresentationModel = require( 'br/presenter/PresentationModel' );
+var Field = require( 'br/presenter/node/Field' );
 
-  var br = require( 'br' );
-  var ServiceRegistry = require( 'br/ServiceRegistry' );
-  var PresentationModel = require( 'br/presenter/PresentationModel' );
-  
-  function ExamplePresentationModel() {
-    this.todoText = new br.presenter.node.Field( '' );
-    this.eventHub = ServiceRegistry.getService( 'br.demo-event-hub' );
+function ExamplePresentationModel() {
+  this.todoText = new Field( '' );
+  this.eventHub = ServiceRegistry.getService( 'br.demo-event-hub' );
+}
+br.extend( ExamplePresentationModel, PresentationModel );
+
+ExamplePresentationModel.prototype.keyPressed = function( data, event ) {
+  if( event.keyCode === ENTER_KEY_CODE ) {
+    var todoTextValue = this.todoText.value.getValue();
+    this.eventHub.channel( 'todo-list' ).trigger( 'todo-added', { text: todoTextValue } );
+    this.todoText.value.setValue( '' );
   }
-  br.Core.extend( ExamplePresentationModel, PresentationModel );
-  
-  ExamplePresentationModel.prototype.keyPressed = function( data, event ) {
-    if( event.keyCode === ENTER_KEY_CODE ) {
-      var todoTextValue = this.todoText.value.getValue();
-      this.eventHub.channel( 'todo-list' ).trigger( 'todo-added', { text: todoTextValue } );
-      this.todoText.value.setValue( '' );
-    }
 
-    return true;
-  };
-  
-  brjstodo.todo.todoinput.ExamplePresentationModel = ExamplePresentationModel;
+  return true;
+};
 
-} )();
+module.exports = ExamplePresentationModel;
--- todo-bladeset/blades/todoitems/src/brjstodo/todo/todoitems/ExamplePresentationModel.js
+++ todo-bladeset/blades/todoitems/src/brjstodo/todo/todoitems/ExamplePresentationModel.js
@@ -1,37 +1,34 @@
-( function() {
-  
-  var br = require( 'br/Core' );
-  var ServiceRegistry = require( 'br/ServiceRegistry' );
-  var PresentationModel = require( 'br/presenter/PresentationModel' );
-  var DisplayField = require( 'br/presenter/node/DisplayField' );
-  var NodeList = require( 'br/presenter/node/NodeList' );
+var br = require( 'br/Core' );
+var ServiceRegistry = require( 'br/ServiceRegistry' );
+var PresentationModel = require( 'br/presenter/PresentationModel' );
+var DisplayField = require( 'br/presenter/node/DisplayField' );
+var NodeList = require( 'br/presenter/node/NodeList' );
 
-  function ExamplePresentationModel() {
-    this.items = new NodeList( [ new DisplayField( 'foo' ), new DisplayField( 'bar' ) ] );
+function ExamplePresentationModel() {
+  this.items = new NodeList( [ new DisplayField( 'foo' ), new DisplayField( 'bar' ) ] );
 
-    // get the event hub
-    this.eventHub = ServiceRegistry.getService( 'br.demo-event-hub' );
+  // get the event hub
+  this.eventHub = ServiceRegistry.getService( 'br.demo-event-hub' );
 
-    // register to recieve events
-    this.eventHub.channel( 'todo-list' ).on( 'todo-added', this._todoAdded, this );
-  }
+  // register to recieve events
+  this.eventHub.channel( 'todo-list' ).on( 'todo-added', this._todoAdded, this );
+}
 
-  br.Core.extend( ExamplePresentationModel, PresentationModel );
+br.Core.extend( ExamplePresentationModel, PresentationModel );
 
-  ExamplePresentationModel.prototype._todoAdded = function( added ) {
+ExamplePresentationModel.prototype._todoAdded = function( added ) {
 
-    // create a new field for the new item
-    var newItem = new DisplayField( added.text );
+  // create a new field for the new item
+  var newItem = new DisplayField( added.text );
 
-    // get the existing items
-    var nodes = this.items.getPresentationNodesArray();
+  // get the existing items
+  var nodes = this.items.getPresentationNodesArray();
 
-    // append the new item to the array
-    nodes.push( newItem );
+  // append the new item to the array
+  nodes.push( newItem );
 
-    // update the View Model which triggers a UI update
-    this.items.updateList( nodes );
-  };
+  // update the View Model which triggers a UI update
+  this.items.updateList( nodes );
+};
 
-  brjstodo.todo.todoitems.ExamplePresentationModel = ExamplePresentationModel;
-} )();
+module.exports = ExamplePresentationModel;
