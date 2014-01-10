d4fc26693cb9042e199d14b9f8b744d451c9ca08
Phil Leggetter <phil@leggetter.co.uk>
2014-01-10 15:57:57 +0000

    fix missing requires

--- default-aspect/src/brjstodo/App.js
+++ default-aspect/src/brjstodo/App.js
@@ -1,14 +1,16 @@
 var PresenterComponent = require( 'br/presenter/component/PresenterComponent' );
 var SimpleFrame = require( 'br/component/SimpleFrame' );
+var TodoInputModel = require( 'brjstodo/todo/todoinput/ExamplePresentationModel' );
+var TodoItemsModel = require( 'brjstodo/todo/todoitems/ExamplePresentationModel' );
 
 var App = function() {
 
   // todo input Blade
-  var inputModel = new brjstodo.todo.todoinput.ExamplePresentationModel();
+  var inputModel = new TodoInputModel();
   this.inputComponent = new PresenterComponent( 'brjstodo.todo.todoinput.view-template', inputModel );
 
   // todo items Blade
-  var itemsModel = new brjstodo.todo.todoitems.ExamplePresentationModel();        
+  var itemsModel = new TodoItemsModel();        
   this.itemsComponent = new PresenterComponent( 'brjstodo.todo.todoitems.view-template', itemsModel );
 
   var inputFrame = new SimpleFrame( this.inputComponent );
