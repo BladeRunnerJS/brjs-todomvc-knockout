17ba303798770e01974a3d7d760d3a864f88eed2
Phil Leggetter <phil@leggetter.co.uk>
2014-02-14 19:53:01 +0000

    Tidy up

--- default-aspect/src/brjstodo/App.js
+++ default-aspect/src/brjstodo/App.js
@@ -1,5 +1,4 @@
 var PresenterComponent = require( 'br/presenter/component/PresenterComponent' );
-var SimpleFrame = require( 'br/component/SimpleFrame' );
 var TodoInputViewModel = require( 'brjstodo/todo/todoinput/TodoInputViewModel' );
 var TodoItemsViewModel = require( 'brjstodo/todo/todoitems/TodoItemsViewModel' );
 
@@ -7,17 +6,13 @@ var App = function() {
 
   // todo input Blade
   var inputModel = new TodoInputViewModel();
-  this.inputComponent = new PresenterComponent( 'brjstodo.todo.todoinput.view-template', inputModel );
+  var inputComponent = new PresenterComponent( 'brjstodo.todo.todoinput.view-template', inputModel );
+  document.getElementById( 'header' ).appendChild( inputComponent.getElement() );
 
   // todo items Blade
   var itemsModel = new TodoItemsViewModel();        
-  this.itemsComponent = new PresenterComponent( 'brjstodo.todo.todoitems.view-template', itemsModel );
-
-  var inputFrame = new SimpleFrame( this.inputComponent );
-  document.getElementById( 'header' ).appendChild( inputFrame.getElement() );
-
-  var itemsFrame = new SimpleFrame( this.itemsComponent );
-  document.getElementById( 'main' ).appendChild( itemsFrame.getElement() );
+  var itemsComponent = new PresenterComponent( 'brjstodo.todo.todoitems.view-template', itemsModel );
+  document.getElementById( 'main' ).appendChild( itemsComponent.getElement() );
 };
 
 module.exports = App;
