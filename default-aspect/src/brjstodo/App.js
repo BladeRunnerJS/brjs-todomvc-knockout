fd17941ab9b0b9d71dfd60caca2050bd9e1cfa8d
leggetter <phil@leggetter.co.uk>
2014-02-21 18:26:22 +0000

    aspect updated to use new KnockoutComponents

--- default-aspect/src/brjstodo/App.js
+++ default-aspect/src/brjstodo/App.js
@@ -1,4 +1,4 @@
-var PresenterComponent = require( 'br/presenter/component/PresenterComponent' );
+var KnockoutComponent = require( 'br/knockout/KnockoutComponent' );
 var TodoInputViewModel = require( 'brjstodo/todo/todoinput/TodoInputViewModel' );
 var TodoItemsViewModel = require( 'brjstodo/todo/todoitems/TodoItemsViewModel' );
 
@@ -16,13 +16,13 @@ var App = function() {
 
   // todo input Blade
   var inputModel = new TodoInputViewModel();
-  var inputComponent = new PresenterComponent( 'brjstodo.todo.todoinput.view-template', inputModel );
+  var inputComponent = new KnockoutComponent( 'brjstodo.todo.todoinput.view-template', inputModel );
   var inputEl = inputComponent.getElement().children[ 0 ];
   todoAppEl.appendChild( inputEl );
 
   // todo items Blade
   var itemsModel = new TodoItemsViewModel();
-  var itemsComponent = new PresenterComponent( 'brjstodo.todo.todoitems.view-template', itemsModel );
+  var itemsComponent = new KnockoutComponent( 'brjstodo.todo.todoitems.view-template', itemsModel );
   var itemsEl = itemsComponent.getElement().children[ 0 ];
   todoAppEl.appendChild( itemsEl );
 
