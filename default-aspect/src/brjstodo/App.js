21931dd265b83baa0b57d7333da25ff62c65234c
leggetter <phil@leggetter.co.uk>
2014-02-27 18:25:39 +0000

    fix based on KnockoutComponent change

--- default-aspect/src/brjstodo/App.js
+++ default-aspect/src/brjstodo/App.js
@@ -7,30 +7,22 @@ var App = function() {
 
   var todoAppEl = document.getElementById( 'todoapp' );
 
-  /*
-  Note:
-  Templates are retrieved from the HTMLService using an element ID. Since TodoMVC
-  requires parts of the HTML to have uniqueID and those IDs are for what we could
-  consider the Blades of the TodoMVC app we have to wrap the Blade templates
-  in a parent element. Hence, the calls to component.getElement().children[0].
-  */
-
   // todo input Blade
   var inputModel = new TodoInputViewModel();
   var inputComponent = new KnockoutComponent( 'brjstodo.todo.todoinput.view-template', inputModel );
-  var inputEl = inputComponent.getElement().children[ 0 ];
+  var inputEl = inputComponent.getElement();
   todoAppEl.appendChild( inputEl );
 
   // todo items Blade
   var itemsModel = new TodoItemsViewModel();
   var itemsComponent = new KnockoutComponent( 'brjstodo.todo.todoitems.view-template', itemsModel );
-  var itemsEl = itemsComponent.getElement().children[ 0 ];
+  var itemsEl = itemsComponent.getElement();
   todoAppEl.appendChild( itemsEl );
 
   // todo filter/footer
   var filterModel = new TodoFilterViewModel();
   var filterComponent = new KnockoutComponent( 'brjstodo.todo.todofilter.view-template', filterModel );
-  var filterEl = filterComponent.getElement().children[ 0 ];
+  var filterEl = filterComponent.getElement();
   todoAppEl.appendChild( filterEl );
 };
 
